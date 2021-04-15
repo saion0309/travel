5de107bdaa0cee4a9d0790cd3c1115c9523f5df7
chen <chen20000616@gmail.com>
2021-04-15 17:12:18 +0800

    add header

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,51 @@
+<template>
+	<div id="header">
+		<div class="header-left"><i class="iconfont">&#xe667;</i></div>
+		<div class="header-input">
+			<input type="text" class="iconfont" placeholder=" &#xe74b; 输入城市/景点/游玩主题">
+		</div>
+		<div class="header-right">
+			<span>城市</span>
+			<i class="iconfont">&#xe62a;</i>
+		</div>
+	</div>
+</template>
+<script>
+
+</script>
+<style lang="less" scoped>
+	#header {
+		display: flex;
+		// height: .86rem;
+		line-height: .62rem;
+		background: #00bcd4;
+		color: #fff;
+		padding: .12rem;
+
+		.header-left {
+			width: .64rem;
+			float: left;
+		}
+
+		.header-input {
+			flex: 1;
+			margin-left: .2rem;
+			input{
+				width: calc(100% - 0.2rem);
+				height: 100%;
+				outline: none;
+				border: none;
+				background: #fff;
+				border-radius: .1rem;
+				padding-left:.2rem ;
+				
+			}
+		}
+
+		.header-right {
+			width: 1.24rem;
+			float: right;
+			text-align: center;
+		}
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<HomeHeader></HomeHeader>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+	import HomeHeader from '../components/HomeHeader.vue'
+	export default{
+		name:'Home',
+		data(){},
+		components:{
+			HomeHeader
+		}
+		
+	}
 </script>
+
+<style scoped>
+	
+</style>
